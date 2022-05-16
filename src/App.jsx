e006fa770acdb39ca48f37a8f644279b36a2559a
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-16 10:02:24 +0200

    added Employees component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.scss";
 import { Customers } from "../components/Customers";
-
+import { Employees } from "../components/Employees";
 function App() {
   return (
     <div className="App">
       <h1>Text Question</h1>
       <Customers />
+      <Employees />
     </div>
   );
 }
