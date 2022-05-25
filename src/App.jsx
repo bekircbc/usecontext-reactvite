1355a64f7bcd226031cbbe9e89c20392047c5c50
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-26 00:37:38 +0200

    icon added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import { Employees } from "../components/Employees";
 function App() {
   return (
     <div className="App">
-      <h1>Test Question</h1>
+      <link rel="icon" type="image/png" href="images/icon.png" />{" "}
+      <div className="title">
+        <img src="images/icon.png" alt="logo" />
+        <h1>Use Context React Vite</h1>
+      </div>
       <Customers />
       <Employees />
     </div>
